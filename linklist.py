c45a93654ee20bb2998a4f6cc848968f7bb0195d
Ali Sina <alisinasultani255@gmail.com>
2023-01-06 21:57:06 +0430

    perpend and pop form first and get  created

--- linklist.py
+++ linklist.py
@@ -49,14 +49,56 @@ class Linklist:
             self.head = None
             self.tail  = None
         return temp
-        
-            
     
+    def Perpend(self, value):
+        new_node = Node(value)
+        if self.lenght == 0:
+            self.head = new_node
+            self.tail = new_node
+        
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        
+        self.lenght += 1
+        return  True
+    
+    def Pop_first(self):
+        if self.lenght == 0:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.lenght -= 1
             
+            if self.lenght ==0:
+                
+                self.tail = None
+                
+            return temp
+    
+    
+    def Get(self, index):
+        if index < 0 or index >= self.lenght:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp.value
+        
+        
+        
+        
 first = Linklist(11)
 first.Append(1)
 first.Append(100)
 first.Append(200)
 first.Append(4234)
-first.Pop()
+first.Perpend(0)
+# first.Perpend(0.121)
+first.Pop_first()
+# first.Pop()
+first.Perpend(0)
+print(first.Get(3))
 first.Print_lsit()
